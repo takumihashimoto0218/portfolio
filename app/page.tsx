f160dc8e72011f67ab3242697b0ceaebf243185a
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-10-02 03:05:19 +0900

    introductionコンポーネントを作成し表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
+import Introduction from '@/components/Introduction'
 
 export default function Home() {
   return (
     <>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>リンク</h1>
-        <h1>スキル</h1>
+        <Introduction />
       </div>
     </>
   )

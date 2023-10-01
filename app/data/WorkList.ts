ca7a45eefc730edb75d37f8bd4b6c590ee35ff39
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-10-02 04:46:14 +0900

    woks作成

---
+++ app/data/WorkList.ts
@@ -0,0 +1,50 @@
+export type Work = {
+  imagePath: string; // imgからの相対パス
+  imageHeight: number;
+  cardWidth: number;
+  title: string;
+  description: string;
+  tips: string[];
+};
+
+const WorkList: Work[] = [
+  {
+    imagePath: "portfolio.png",
+    imageHeight: 225,
+    cardWidth: 350,
+    title: "portfolioサイト",
+    description:
+      "このサイトです。\nDBをsupabaseに格納したい \n https://github.com/yusei53/yusei53-portfolio",
+    tips: ["Next.js", "TypeScript"],
+  },
+  {
+    imagePath: "asap.png",
+    imageHeight: 225,
+    cardWidth: 350,
+    title: "trello",
+    description:
+      "trello",
+    tips: ["React"],
+  },
+  {
+    imagePath: "zundamon.png",
+    imageHeight: 225,
+    cardWidth: 350,
+    title: "atorie",
+    description:
+      "atorie",
+    tips: ["React", "flask", "ChatGPT API"],
+  },
+  {
+    imagePath: "typro.png",
+    imageHeight: 225,
+    cardWidth: 350,
+    title: "ainavi",
+    description:
+      "ainavi",
+    tips: ["React", "TypeScript"],
+  },
+];
+
+export default WorkList;
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Introduction from '@/components/Introduction'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Introduction />
+        <Link href="/works">Works</Link>
       </div>
     </>
   )

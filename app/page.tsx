de4ba5800687a595140332550f462c6d44e09f39
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-10-02 07:48:33 +0900

    rootcomponentを整理

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 import Introduction from '@/components/Introduction'
-import Link from 'next/link'
+import PageLink from '@/components/PageLink'
 
 export default function Home() {
   return (
     <>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Introduction />
-        <Link href="/works">Works</Link>
-        <Link href="/skills">Skill</Link>
-        <Link href="/about">About</Link>
+        <PageLink />
       </div>
     </>
   )

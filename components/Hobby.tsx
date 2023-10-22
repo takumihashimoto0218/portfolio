5163b53ae8f11fbcf9513f0f499893977dbb150a
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-10-22 21:52:15 +0900

    趣味をカードで囲う

--- components/Hobby.tsx
+++ components/Hobby.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { Hobby as HobbyType } from '@/app/data/HobbyList';
+import { Card } from '@mui/material';
 
 type HobbyProps = {
   hobbyData: HobbyType;
@@ -8,18 +9,20 @@ type HobbyProps = {
 
 const Hobby: React.FC<HobbyProps> = ({ hobbyData }) => {
   return (
-    <div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <Image
-          src={`/img/hobbyimage/${hobbyData.imagePath}`}
-          alt={hobbyData.title} 
-          height={hobbyData.imageHeight} 
-          width={hobbyData.cardWidth} 
-        />
+    <Card sx={{ height: '100%', backgroundColor: 'whitesmoke' }}>
+      <div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <Image
+            src={`/img/hobbyimage/${hobbyData.imagePath}`}
+            alt={hobbyData.title} 
+            height={hobbyData.imageHeight} 
+            width={hobbyData.cardWidth} 
+          />
+        </div>
+        <h2 className="text-center my-3">{hobbyData.title}</h2>
+        <p className="text-center my-2">{hobbyData.description}</p>
       </div>
-      <h2 className="text-center my-3">{hobbyData.title}</h2>
-      <p className="text-center my-2">{hobbyData.description}</p>
-    </div>
+    </Card>
   );
 }
 
